4f734c3a8d5d41a3ec0ab1df38a6b546e5a36e50
twilroad <269044570@qq.com>
2017-05-27 19:58:25 +0800

    add permission group register

--- src/ModuleServiceProvider.php
+++ src/ModuleServiceProvider.php
@@ -10,6 +10,7 @@ namespace Notadd\Socket;
 
 use Illuminate\Events\Dispatcher;
 use Notadd\Foundation\Module\Abstracts\Module;
+use Notadd\Socket\Listeners\PermissionGroupRegister;
 use Notadd\Socket\Listeners\PermissionTypeRegister;
 
 /**
@@ -22,6 +23,7 @@ class ModuleServiceProvider extends Module
      */
     public function boot()
     {
+        $this->app->make(Dispatcher::class)->subscribe(PermissionGroupRegister::class);
         $this->app->make(Dispatcher::class)->subscribe(PermissionTypeRegister::class);
     }
 
