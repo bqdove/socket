26066724dd5030740bd0cdeea142ceaaf3b723d6
twilroad <269044570@qq.com>
2017-05-28 17:21:03 +0800

    fix organization

--- src/Commands/SocketServerCommand.php
+++ src/Commands/SocketServerCommand.php
@@ -3,7 +3,7 @@
  * This file is part of Notadd.
  *
  * @author TwilRoad <heshudong@ibenchu.com>
- * @copyright (c) 2016, iBenchu.org
+ * @copyright (c) 2016, notadd.com
  * @datetime 2016-10-17 16:04
  */
 namespace Notadd\Socket\Commands;
